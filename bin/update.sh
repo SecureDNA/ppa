40fd8cf52a8f8fc45317290db6838455f3469a88
Martin Kysel <code@martinkysel.com>
2023-11-15 11:13:13 -0500

    add license tooling and restructure

---
+++ bin/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -v
+
+export KEYNAME=packaging@securedna.org
+
+(
+    set -e
+    set -v
+
+    cd ../deb/
+
+    # Packages & Packages.gz
+    dpkg-scanpackages --multiversion . > Packages
+    gzip -k -f Packages
+
+    # Release, Release.gpg & InRelease
+    apt-ftparchive release . > Release
+    gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
+    gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
+)
